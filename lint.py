ce720f9b3b511f8e1134f8b700566614ab20da06
potato <Admin@MIS>
2023-04-04 12:02:13 +0100

    linting

---
+++ lint.py
@@ -0,0 +1,19 @@
+"""
+
+function to count in a sequence
+
+"""
+def count(sequence, item):
+    """
+
+    this counts
+
+    """
+    counter = 0
+
+    for num in sequence:
+
+        if num == item:
+
+            counter += 1
+    return counter
